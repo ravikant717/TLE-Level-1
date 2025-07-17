7a6f21ba42b6dcaa9b05516b77e3473dde59abaf
Ravikant Munda <rxn114392@gmail.com>
2025-07-17 06:54:33 +0530

    Update Different Divisors.cpp

--- Number Theory/Codeforces/Different Divisors.cpp
+++ Number Theory/Codeforces/Different Divisors.cpp
@@ -1,41 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-
-void solve() {
-    int d; 
-    cin >> d; 
-    vector<ll> primes; 
-    for(int i = 2; i <= 10000; i++) {
-        bool is_prime = true; 
-        //SQRT Method for prime check
-        for(ll j = 2; j * j <= i; j++) {
-            if(i % j == 0) {
-                is_prime = false; 
-                break; 
-            }
-        }
-        if(is_prime) primes.push_back(i); 
-
-    }
-    ll p = *lower_bound(primes.begin(), primes.end(), d + 1); 
-    ll q = *lower_bound(primes.begin(), primes.end(), d + p); 
-    ll a = min(p * q, p * p * p); 
-
-    cout << a << "\n"; 
-}
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
     int t;
-    cin >> t; 
+    cin >> t;
 
-    while(t--) {
-        solve(); 
+    vector<long long> primes;
+    for (long long i = 2; i <= 100000; i++)
+    {
+        bool is_prime = true;
+        for (long long j = 2; j * j <= i; j++)
+        {
+            if (i % j == 0)
+            {
+                is_prime = false;
+                break;
+            }
+        }
+        if (is_prime)
+            primes.push_back(i);
+    }
 
-    } 
-    return 0;
+    while (t--)
+    {
+        long long d;
+        cin >> d;
+
+        long long p = *lower_bound(primes.begin(), primes.end(), d + 1);
+        long long q = *lower_bound(primes.begin(), primes.end(), p + d);
+
+        cout << p * q << "\n";
+    }
 }
